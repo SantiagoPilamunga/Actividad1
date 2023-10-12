01f75d96a7620a6423106af4007aa388e7eb871f
Santiago Pilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-12 16:09:52 -0500

    cambio1

--- sumagauss.c
+++ sumagauss.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-int main(int argc, char const *argv[])
+int main()
 {
+    int num,i=1;
+    int suma=1;
+    printf("Ingrese un numero");
+    scanf("%d",&num);
+    if (num<0)
+    {
+        printf("la operacion no se puede realizar");
+    }else{
+        while (i!=num)
+    {
+        /* code */
+    }
+
+    }
+    
+    
     
     return 0;
 }
