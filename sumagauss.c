aee705684d79769001d32ab155696cca9c1a616e
Santiago Pilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-12 16:37:41 -0500

    programa con for

--- sumagauss.c
+++ sumagauss.c
@@ -3,6 +3,7 @@ int main()
 {
     int num,i=1;
     int suma=0;
+    int suma2=0;
     printf("Ingrese un numero: \n");
     scanf("%d",&num);
     if (num<0)
@@ -16,8 +17,15 @@ int main()
         i=i+1;
     }
         printf("La suma de gauss del numero %d es: %d \n",num,suma);
+        //procedimiento con for 
+        for ( i = 1; i <=num; i++)
+        {
+            suma2+=i;
+        }
+        printf("Con for La suma de gauss del numero %d es: %d \n",num,suma2);
     }
     printf("Gracias por usar el programa \n");
+    
     
     
     return 0;
