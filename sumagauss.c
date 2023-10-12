169fc72e970337b1d0773b7349cbed6675665c90
Santiago Pilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-12 16:18:46 -0500

    cambio3

--- sumagauss.c
+++ sumagauss.c
@@ -3,19 +3,19 @@ int main()
 {
     int num,i=1;
     int suma=0;
-    printf("Ingrese un numero");
+    printf("Ingrese un numero: \n");
     scanf("%d",&num);
     if (num<0)
     {
         printf("la operacion no se puede realizar");
     }else{
-        while (i!=num)
+        while (i<=num)
     {
         
         suma+=i;
         i=i+1;
     }
-
+        printf("la suma del numero %d es: %d",num,suma);
     }
     
     
